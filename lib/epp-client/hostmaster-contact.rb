66b1a24dfc9629450f2b1f2461e953e0bc962fdf
Yuriy Kolodovskyy <kolodovskyy@ukrindex.com>
2012-11-19 00:50:58 +0200

    Contact object done

--- lib/epp-client/hostmaster-contact.rb
+++ lib/epp-client/hostmaster-contact.rb
@@ -35,7 +35,7 @@ module EPPClient
               xml.contact :id, 'autonic'
             end
             contact[:postalInfo].each do |type,infos|
-              xml.contact :postalInfo, :type => type do
+              xml.contact :postalInfo, type: type do
                 xml.contact :name, infos[:name]
                 xml.contact :org, infos[:org] if infos.key?(:org)
                 xml.contact :addr do
@@ -56,10 +56,10 @@ module EPPClient
               xml.contact :pw, contact[:authInfo]
             end
             if contact.key?(:disclose)
-              xml.contact :disclose, flag: '1' do
+              xml.contact :disclose, flag: '1' do # TODO: parameter for flag need
                 contact[:disclose].each do |disc|
                   if disc.key?(:type)
-                    xml.contact disc[:name], :type => disc[:type]
+                    xml.contact disc[:name], type: disc[:type]
                   else
                     xml.contact disc[:name]
                   end
@@ -81,5 +81,69 @@ module EPPClient
       end
     end
 
+    def contact_update_xml(args)
+      command do |xml|
+        xml.update do
+          xml.contact :update, 'xmlns:contact' => EPPClient::SCHEMAS_URL['contact-1.1'] do
+            xml.contact :id, args[:id]
+            if args.key?(:add) && args[:add].key?(:status)
+              xml.contact :add do
+                args[:add][:status].each do |s|
+                  xml.contact :status, s: s
+                end
+              end
+            end
+            if args.key?(:rem) && args[:rem].key?(:status)
+              xml.contact :rem do
+                args[:rem][:status].each do |s|
+                  xml.contact :status, s: s
+                end
+              end
+            end
+            if args.key?(:chg)
+              contact = args[:chg]
+              xml.contact :chg do
+                if contact.key?(:postalInfo)
+                  contact[:postalInfo].each do |type,infos|
+                    xml.contact :postalInfo, type: type do
+                      xml.contact :name, infos[:name]
+                      xml.contact :org, infos[:org] if infos.key? :org
+                      xml.contact :addr do
+                        infos[:addr][:street].each do |street|
+                          xml.contact :street, street
+                        end
+                        xml.contact :city, infos[:addr][:city]
+                        xml.contact :sp, infos[:addr][:sp] if infos[:addr].key? :sp
+                        xml.contact :pc, infos[:addr][:pc] if infos[:addr].key? :pc
+                        xml.contact :cc, infos[:addr][:cc]
+                      end
+                    end
+                  end
+                end
+                [:voice, :fax, :email].each do |val|
+                  xml.contact val, contact[val] if contact.key? val
+                end
+                if contact.key?(:authInfo)
+                  xml.contact :authInfo do
+                    xml.contact :pw, contact[:authInfo]
+                  end
+                end
+                if contact.key?(:disclose)
+                  xml.contact :disclose, flag: '1' do # TODO: parameter for flag need
+                    contact[:disclose].each do |disc|
+                      if disc.key?(:type)
+                        xml.contact disc[:name], type: disc[:type]
+                      else
+                        xml.contact disc[:name]
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
