25b7787536233ae5b3be4f5debc3e0b31b5fb04b
Yuriy Kolodovskyy <kolodovskyy@ukrindex.com>
2012-11-20 13:49:08 +0200

    Fix bug with concatination xml with length of packet

--- lib/epp-client/hostmaster-connection.rb
+++ lib/epp-client/hostmaster-connection.rb
@@ -2,7 +2,8 @@ module EPPClient
   module HostmasterConnection
     def send_frame(xml)
       @sent_frame = xml
-      @socket.write([xml.bytesize + 4].pack("N") + xml)
+      @socket.write [xml.bytesize + 4].pack("N")
+      @socket.write xml
       sent_frame_to_xml
     end
   end
