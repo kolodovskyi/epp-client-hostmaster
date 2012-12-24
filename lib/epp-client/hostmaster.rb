a3c3dec3f42e698885e222050dc44d6298f20113
Yuriy Kolodovskyy <kolodovskyy@ukrindex.com>
2012-12-24 20:21:34 +0200

    0.2.2

--- lib/epp-client/hostmaster.rb
+++ lib/epp-client/hostmaster.rb
@@ -9,7 +9,7 @@ require "epp-client/hostmaster-transfer"
 
 module EPPClient
   class Hostmaster < Base
-    VERSION = '0.2.1'
+    VERSION = '0.2.2'
 
     SCHEMAS = %w(domain-1.1 host-1.1 contact-1.1 rgp-1.1)
 
